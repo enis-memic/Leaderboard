025e4f2c15cd86294c7f30ceaa0a6c469099ef34
Enis <enis.mema@gmail.com>
2023-03-07 13:35:20 +0100

    Fix bugs

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,15 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  devServer: {
-    static: './dist',
-  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
